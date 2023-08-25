39aab9df6d62e9b98f98191634aed690ace541b4
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-08-25 14:00:41 +0200

    added titles

--- nextapp/app/jobs/search/page.tsx
+++ nextapp/app/jobs/search/page.tsx
@@ -1,21 +1,36 @@
+"use client"
+
+import { useState, useEffect } from 'react';
 import { IJobs } from "@/app/models/IJobs";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 
+export default function Search() {
+    const [data, setData] = useState<IJobs[]>([]); 
 
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch("http://localhost:3000/api/jobs");
+                const responseData = await response.json();
+                setData(responseData); 
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
 
-export default async function Search() {
-   
-
-    const response = await fetch("http://localhost:3000/api/jobs")
-    const data = await response.json()
-    console.log(data)
+        fetchData(); 
+    }, []);
 
     return (
         <>
-        <div>
-            {JSON.stringify(data)}
-        </div>
+            <div>
+                {data.map((job) => (
+                    <div key={job.id}> 
+                        <h2>{job.title}</h2> 
+                    </div>
+                ))}
+            </div>
         </>
-    )
+    );
 }
 
